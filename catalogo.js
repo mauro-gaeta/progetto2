b03f0d40ec7f31287996898e7533307c8e61b996
mauro-gaeta <g_mauro@msn.com>
2024-06-24 16:52:31 +0200

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const letterTableBody = document.getElementById('letter-table-body');
 
-    // Array dei file XML delle lettere (puoi aggiungere altri file XML qui)
+    // Array dei file XML delle lettere
     const letterFiles = [
         'letters/letter1.xml',
-        'letters/letter2.xml',
+        'letters/letter2.xml'
         // Aggiungi altri file XML qui
     ];
 
@@ -13,12 +13,25 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(response => response.text())
             .then(data => {
                 const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(data, "text/xml");
+                const xmlDoc = parser.parseFromString(data, "application/xml");
 
-                const title = xmlDoc.getElementsByTagName("title")[0].textContent;
-                const date = xmlDoc.getElementsByTagName("date")[0].textContent;
-                const recipient = xmlDoc.getElementsByTagName("recipient")[0].textContent;
-                const description = `Lettera di Aldo Moro a ${recipient}`;
+                let title, date, recipient, description;
+
+                if (xmlDoc.documentElement.nodeName === "TEI") {
+                    // Parsing per file TEI
+                    title = xmlDoc.getElementsByTagName("title")[0].textContent;
+                    date = xmlDoc.getElementsByTagName("date")[0].textContent;
+                    recipient = xmlDoc.getElementsByTagName("sourceDesc")[0].textContent.split(" a ")[1];
+                    description = `Lettera di Aldo Moro a ${recipient}`;
+                } else if (xmlDoc.documentElement.nodeName === "metadata") {
+                    // Parsing per file Dublin Core
+                    title = xmlDoc.getElementsByTagName("title")[0].textContent;
+                    date = xmlDoc.getElementsByTagName("date")[0].textContent;
+                    recipient = xmlDoc.getElementsByTagName("subject")[0].textContent.split(" a ")[1];
+                    description = `Lettera di Aldo Moro a ${recipient}`;
+                } else {
+                    throw new Error("Formato XML sconosciuto");
+                }
 
                 const row = document.createElement("tr");
 
